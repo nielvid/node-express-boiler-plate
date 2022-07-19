1b66ca159ae1e4478199c2376e327b0004d33070
Nielvid <56310246+nielvid@users.noreply.github.com>
2022-07-19 15:17:14 +0100

    linting updated

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-import express from 'express';
+import express from 'express'
 // rest of the code remains same
-const app = express();
-const PORT = 8000;
-app.get('/', (req, res, next) => res.send('Express Server is not runnning  fine'));
-
+const app = express()
+const PORT = process.env.PORT || 8000
+app.get('/', (req, res, next) =>
+	res.send('Express Server is not runnning  fine')
+)
 app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
-});
+	console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`)
+})
